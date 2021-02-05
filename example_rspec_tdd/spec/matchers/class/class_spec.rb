37e3af7356cd70119f6dcd41418689f590d2361b
John Diego <johnoliveirasilva9@gmail.com>
2021-02-04 23:49:32 -0200

    Respond_to, Be_an and Be_a

--- example_rspec_tdd/spec/matchers/class/class_spec.rb
+++ example_rspec_tdd/spec/matchers/class/class_spec.rb
@@ -3,11 +3,26 @@ describe "Classes" do
    it "be_instance_of" do
     expect(10).to be_instance_of(Integer) # Exact the class
   end
-  
+
   it "be_king_of" do
     str = StringNotEmpty.new
     expect(str).to be_kind_of(String)
     expect(str).to be_kind_of(StringNotEmpty) 
     expect(10).to be_kind_of(Integer) # Pode ser por herança
   end
+  
+  it "respond_to" do
+    expect("ruby").to respond_to(:size) 
+    expect("ruby").to respond_to(:count )
+  end
+  
+  it "be_an / be_a" do
+    str = StringNotEmpty.new
+    expect(str).to be_an(String)
+    expect(str).to be_an(StringNotEmpty)
+
+    expect(str).to be_a(String) 
+    expect(str).to be_a(String) 
+  end
+  
 end
